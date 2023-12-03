19b4f1a049d564512da1c3c27897408d88ba75a3
devopsdbroe <dbroe1995@gmail.com>
2023-12-03 11:09:15 -0500

    Implement footer section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<Outlet />
+			<Footer />
 		</>
 	);
 }
