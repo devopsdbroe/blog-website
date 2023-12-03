48da8b2e6c667cf2868fcf552b786d421d80f3d7
devopsdbroe <dbroe1995@gmail.com>
2023-12-03 12:07:32 -0500

    Implement modal for sign in

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import { NavLink } from "react-router-dom";
 import { FaBars, FaDribbble, FaFacebook, FaTwitter } from "react-icons/fa";
 import { FaXmark } from "react-icons/fa6";
 import { useState } from "react";
+import Modal from "./Modal";
 
 const Navbar = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	const toggleMenu = () => {
 		setIsMenuOpen(!isMenuOpen);
@@ -18,6 +20,16 @@ const Navbar = () => {
 		{ path: "/contact", link: "Contact" },
 		{ path: "/services", link: "Services" },
 	];
+
+	// Modal details
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
+
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
+
 	return (
 		<header className="bg-black text-white fixed top-0 left-0 right-0">
 			<nav className="p-4 max-w-7xl mx-auto flex justify-between items-center">
@@ -52,10 +64,16 @@ const Navbar = () => {
 					<a href="/" className="hover:text-orange-500">
 						<FaTwitter />
 					</a>
-					<button className="bg-orange-500 px-6 py-2 font-medium rounded hover:bg-white hover:text-orange-500 transition-all duration-200 ease-in">
+					<button
+						onClick={openModal}
+						className="bg-orange-500 px-6 py-2 font-medium rounded hover:bg-white hover:text-orange-500 transition-all duration-200 ease-in"
+					>
 						Sign In
 					</button>
 				</div>
+
+				{/* Modal component is here */}
+				<Modal isOpen={isModalOpen} onClose={closeModal} />
 
 				{/* Mobile menu button */}
 				<div className="md:hidden">
